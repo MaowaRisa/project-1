3f2aea8f2b3ffbf704b8129b7196e52c983355ad
maowarisa <maowarisa123@gmail.com>
2024-05-16 14:12:30 +1000

    Project setup- Express and typeScript

---
+++ src/app/server.ts
@@ -0,0 +1,12 @@
+import { Server } from 'http'
+import app from './app'
+const PORT = '5009'
+
+let server: Server
+
+async function bootstrap() {
+    server = app.listen(PORT, () => {
+        console.log(`Example app listening on port ${PORT}`)
+    })
+}
+bootstrap()
