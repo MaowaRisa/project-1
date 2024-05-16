cfbfa32378c51a88a884856fdb8bd930a396a37d
maowarisa <maowarisa123@gmail.com>
2024-05-17 09:41:21 +1000

    cover: parser, request, response, middleware and routing in ExpressJS

--- dist/app/app.js
+++ dist/app/app.js
@@ -6,7 +6,40 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.get('/', (req, res) => {
+// Parsers
+app.use(express_1.default.json());
+app.use(express_1.default.text());
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/courses', courseRouter);
+userRouter.get('/create-user', (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "User is successfully created.",
+        data: user
+    });
+});
+courseRouter.post('/create-course', (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: "Successfully created",
+        data: course
+    });
+});
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
+app.get('/', logger, (req, res) => {
     res.send('Hello World Rocks!');
+});
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('Got data');
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,51 @@
-import express, { Request, Response } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 const app = express()
 const port = 3000
+// Parsers
+app.use(express.json())
+app.use(express.text())
 
-app.get('/', (req : Request, res: Response) => {
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/courses', courseRouter)
+
+userRouter.get(
+  '/create-user',
+  (req: Request, res: Response) =>{
+    const user = req.body;
+    console.log(user);
+    res.json({
+      success: true,
+      message: "User is successfully created.",
+      data: user
+    })
+  }
+)
+courseRouter.post(
+  '/create-course',
+  (req: Request, res: Response) =>{
+    const course = req.body
+    console.log(course)
+
+    res.json({
+      success: true,
+      message: "Successfully created",
+      data: course
+    })
+  }
+)
+const logger = (req: Request, res:Request, next: NextFunction) =>{
+  console.log(req.url, req.method, req.hostname);
+  next();
+}
+
+app.get('/',logger, (req : Request, res: Response) => {
   res.send('Hello World Rocks!')
+})
+app.post('/', (req: Request, res:Response)=>{
+  console.log(req.body)
+  res.send('Got data')
 })
 export default app;
